e3a0c85c11ecfbef3ca9fd9dbad68e02c8f39680
BlackzoneV <sergen-1250@hotmail.com>
2022-01-11 14:52:20 +0300

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,42 @@
+fx_version 'cerulean'
+game 'gta5'
+
+Author 'Blackzone#1020'
+
+files {
+  'data/weapons.meta',
+  'data/weaponcombatpdw.meta',
+  'data/weaponcompactrifle.meta',
+  'data/weaponheavypistol.meta',
+  'data/weaponmachinepistol.meta',
+  'data/weaponminismg.meta',
+  'data/weaponrevolver.meta',
+  'data/weapons_carbinerifle_mk2.meta',
+  'data/weapons_doubleaction.meta',
+  'data/weapons_pistol_mk2.meta',
+  'data/weapons_revolver_mk2.meta',
+  'data/weapons_smg_mk2.meta',
+  'data/weapons_snspistol_mk2.meta',
+  'data/weapons_specialcarbine_mk2.meta',
+  'data/weaponsnspistol.meta',
+  'data/weaponspecialcarbine.meta',
+  'data/weaponvintagepistol.meta',
+}
+
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weapons.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weaponcombatpdw.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weaponcompactrifle.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weaponheavypistol.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weaponmachinepistol.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weaponminismg.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weaponrevolver.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weapons_carbinerifle_mk2.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weapons_doubleaction.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weapons_pistol_mk2.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weapons_revolver_mk2.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weapons_smg_mk2.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weapons_snspistol_mk2.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weapons_specialcarbine_mk2.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weaponsnspistol.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weaponspecialcarbine.meta'
+data_file 'WEAPONINFO_FILE_PATCH' 'data/weaponvintagepistol.meta'
